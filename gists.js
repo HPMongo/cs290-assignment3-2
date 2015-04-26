1a5d33f14bc5a3253442e25bc13b975a62183bbe
Huy Pham <phamhu@onid.oregonstate.edu>
2015-04-26 18:46:48 -0500

    Update main page to display favorite and result gists. Able to store gists to local storage.

--- gists.js
+++ gists.js
@@ -1,3 +1,5 @@
+var settings = null; //setting from local storage
+
 function getID() {
 	return this.gistId;
 }
@@ -62,13 +64,75 @@ function getGist() {
 	var gistList = callGitHub(languageSelected);
 }	
 
+
+/* 
+	Display result back in a table using an array input.
+	Elements to generate HTML table were copied from:
+	http://www.mysamplecode.com/2012/04/generate-html-table-using-javascript.html
+*/
 function displayResult(inArray) {
 	var resultBox = document.getElementById("resultTable");
-	var tmpText;
+	var tmpID, rowCount, row;
 	for(var i=0; i<inArray.length; i++) {
-		tmpText = "Description: " + inArray[i].getDesc() + "\n" + "URL:" + inArray[i].getURL() + "\n \n";
-		var tmpNode = document.createTextNode(tmpText);
-		resultBox.appendChild(tmpNode);
+		tmpID = inArray[i].getID();
+		rowCount = resultBox.rows.length;
+		row = resultBox.insertRow(rowCount);
+		row.insertCell(0).innerHTML='<input type="button" value = "Add to Favorite" onClick="Javacsript:addToFav(this)">';
+		row.insertCell(1).innerHTML= inArray[i].getID();
+		row.insertCell(2).innerHTML= inArray[i].getDesc();
+		row.insertCell(3).innerHTML= inArray[i].getURL();
 	}
 }
 
+/*
+	This function will get the gist information from the page then store it to 
+	local storage and delete it from the result table. 
+*/
+function addToFav(obj) {
+	var index = obj.parentNode.parentNode.rowIndex;
+	var	resultBox = document.getElementById("resultTable");
+//Get information from the table
+	var sID = resultBox.rows[index].cells[1].innerHTML;
+	var sDesc = resultBox.rows[index].cells[2].innerHTML;
+	var sURL = resultBox.rows[index].cells[3].innerHTML;
+//Transform it to a gist object
+	var newFav = new gistDetail(sID, sDesc, sURL);
+//Add it to the setting 
+	settings.favorites.push(newFav);
+//Store it to local storage
+	localStorage.setItem('userFavorites', JSON.stringify(settings));
+//Delete the row from the result table
+	resultBox.deleteRow(index);
+}
+
+/*
+	This function will populate the favorite table using setting from the local storage.
+*/
+function loadFavorites() {
+	var favoriteBox = document.getElementById("favoriteTable");
+	var tmpID, rowCount, row;
+	for(var i=0; i<inArray.length; i++) {
+		tmpID = inArray[i].getID();
+		rowCount = resultBox.rows.length;
+		row = resultBox.insertRow(rowCount);
+		row.insertCell(0).innerHTML='<input type="button" value = "Remove from Favorite" onClick="Javacsript:removefromFav(this)">';
+		row.insertCell(1).innerHTML= inArray[i].getID();
+		row.insertCell(2).innerHTML= inArray[i].getDesc();
+		row.insertCell(3).innerHTML= inArray[i].getURL();
+	}
+}
+/*
+	This function will load variables from the local storage.
+	The code was copied from the course demo - CS290 - Spring 2015 - Oregon State
+*/
+window.onload = function() {
+	var favoritesStr = localStorage.getItem('userFavorites');
+	if( favoritesStr === null ) {
+		settings = {'favorites':[]};
+		localStorage.setItem('userFavorites', JSON.stringify(settings));
+	}
+	else {
+		settings = JSON.parse(localStorage.getItem('userFavorites'));
+		//loadFavorites();
+	}
+}
